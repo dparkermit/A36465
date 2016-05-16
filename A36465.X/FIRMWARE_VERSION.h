092cd839f39fba1267fa9e7c4d8137b776d08490
dparkermit <dparkermit@yahoo.com>
2016-05-16 10:49:27 -0700

    91-36465-251 Rev 02
    Build For Production
    Agile Rev 02

--- A36465.X/FIRMWARE_VERSION.h
+++ A36465.X/FIRMWARE_VERSION.h
@@ -1,8 +1,8 @@
 #ifndef __FIRMWARE_VERISON_H
 #define __FIRMWARE_VERISON_H
 
-#define FIRMWARE_AGILE_REV     1
-#define FIRMWARE_BRANCH        1
+#define FIRMWARE_AGILE_REV     2
+#define FIRMWARE_BRANCH        0
 #define FIRMWARE_MINOR_REV     0
 
 
